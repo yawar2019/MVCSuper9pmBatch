ef64dae447e2e77a478b2bbcd5a70e47051b3d7b
yawar2019 <yawarali17@gmail.com>
2021-03-02 22:09:07 +0530

    committed code

---
+++ DatabaseFirstApproach/Models/EmpDept.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseFirstApproach.Models
+{
+    public class EmpDept
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int? EmpSalary { get; set; }
+        public int DeptId { get; set; }
+        public string DeptName { get; set; }
+    }
+}
